ff6ea89e4871a68ae9aeeeaec2ad4a84160fc166
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2018-06-21 12:53:54 +0200

    dummy example

---
+++ example.py
@@ -0,0 +1,17 @@
+import torch
+from layer import Detection
+import torch.nn as nn
+
+model = nn.Sequential(
+    nn.Conv2d(3, 16, kernel_size=3),
+    Detection(16, 5),
+)
+
+optim = torch.optim.SGD(model.parameters(), 0.1)
+
+optim.zero_grad()
+x = torch.rand(2, 3, 50, 50)
+y, _ = model(x)
+
+loss = y.sum()
+loss.backward()
